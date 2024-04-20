367123bbde0ae75e321abb41f3f5e6dd55a22c6d
Xaephare <louisgrace@onmail.com>
2024-04-20 11:40:28 +0200

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,247 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, lib, options, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+      ./main-user.nix
+    ];
+
+  # Set user
+  main-user.enable = true;
+  main-user.userName = "xaephare";
+
+  # Enable fish
+  programs.fish.enable = true;
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable printing
+  services.printing.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Enable sound
+  sound.enable = true;
+
+  services = {
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      pulse.enable = true;
+      alsa = {
+      enable = true;
+        support32Bit = true;
+      };
+    jack.enable = true;
+    };
+  };
+
+  # Set time and date
+  # time.timeZone = "Pacific/Auckland";
+  time.timeZone = "Europe/Paris";
+  networking.timeServers = options.networking.timeServers.default;
+  services.ntp.enable = true;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_NZ.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_NZ.UTF-8";
+    LC_IDENTIFICATION = "en_NZ.UTF-8";
+    LC_MEASUREMENT = "en_NZ.UTF-8";
+    LC_MONETARY = "en_NZ.UTF-8";
+    LC_NAME = "en_NZ.UTF-8";
+    LC_NUMERIC = "en_NZ.UTF-8";
+    LC_PAPER = "en_NZ.UTF-8";
+    LC_TELEPHONE = "en_NZ.UTF-8";
+    LC_TIME = "en_NZ.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "nz";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.xaephare = {
+    isNormalUser = true;
+    description = "main user";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kitty
+      vscodium
+      brave
+      electron_28
+      libsForQt5.kdeconnect-kde
+      telegram-desktop
+      fish
+      neovim
+      virtualbox
+      steam
+      scanmem
+      android-tools
+      legendary-gl
+      stremio
+      bitwarden
+      spotify # unfree
+      inkscape
+      lf
+      xfce.thunar
+      popcorntime
+      qbittorrent
+      rustc
+      cargo
+      python3
+      android-studio
+    ];
+  };
+
+  home-manager = {
+    # Pass inputs to home-manager modules
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "xaephare" = import ./home.nix;
+    };
+  };
+
+  # Enable ssdm
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Enable polkit
+  security.polkit.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    waybar
+    rofi-wayland
+    alsa-lib
+    brightnessctl
+    playerctl
+    pamixer
+    xfce.thunar
+    xfce.tumbler
+    ffmpegthumbnailer
+    wl-clipboard
+    libsForQt5.dolphin
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal
+    wluma
+    hyprpicker
+    clipman
+    mako
+    lxde.lxsession
+    polkit
+    libsForQt5.polkit-kde-agent
+    appimage-run
+    file
+    curl
+    zip
+    unzip
+    xz
+  ];
+
+  # Run appimages
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
+
+  # Steam dependencies
+    hardware.opengl.driSupport32Bit = true;
+    hardware.pulseaudio.support32Bit = true;
+
+  # Allow Unfree Packages
+  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.allowUnfreePredicate = pkg: 
+  #  builtins.elem (lib.getName pkg) [
+  #  ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    pkgs.font-awesome
+    pkgs.nerdfonts
+    pkgs.noto-fonts
+  ];
+
+  # Udev rules:
+  # Bazecor
+  services.udev.extraRules = ''
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="2201", MODE="0666"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="2200", MODE="0666"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="35ef", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="35ef", MODE="0666"
+  '';
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
