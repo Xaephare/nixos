61d28f819307f2e1dbadcfa5a9fbb4a255bfa590
Xaephare <louisgrace@onmail.com>
2024-05-19 19:26:04 +0200

    Add initial kitty terminal configuration with customized appearance settings and fish shell integration

---
+++ kitty-config.nix
@@ -0,0 +1,45 @@
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      confirm_os_window_close = 0;
+      background_opacity = 0;
+      background_blur = 69;
+      foreground = "#E2E0EC";
+      background = "#0B0A10";
+      selection_foreground = "#D9E0EE";
+      selection_background = "#575268";
+      cursor = "#F5E0DC";
+      cursor_text_color = "#1E1E2E";
+      url_color = "#F5E0DC";
+      active_border_color = "#C9CBFF";
+      inactive_border_color = "#575268";
+      bell_border_color = "#FAE3B0";
+      #: black
+      color0 = "#2B273F";
+      color8 = "#61588E";
+      #: red
+      color1 = "#E97193";
+      color9 = "#E97193";
+      #: green
+      color2 = "#AAC5A0";
+      color10 = "#AAC5A0";
+      #: yellow
+      color3 = "#ECE0A8";
+      color11 = "#ECE0A8";
+      #: blue
+      color4 = "#A8C5E6";
+      color12 = "#A8C5E6";
+      #: magenta
+      color5 = "#DFA7E7";
+      color13 = "#DFA7E7";
+      #: cyan
+      color6 = "#A8E5E6";
+      color14 = "#A8E5E6";
+      #: white
+      color7 = "#E2E0EC";
+      color15 = "#E2E0EC";
+    };
+    shellIntegration.enableFishIntegration = true;
+  };
+}
