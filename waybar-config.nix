898b2d0d4488d257250567a9f21073072420b06c
Xaephare <louisgrace@onmail.com>
2024-05-19 19:18:14 +0200

    Refactor Waybar configuration: streamline modules, enhance formatting, and update styles

--- waybar-config.nix
+++ waybar-config.nix
@@ -7,19 +7,10 @@
     settings = {
       mainBar = {
         "layer" = "top";
-        "modules-left" = ["hyprland/workspaces" "mpd" "hyprland/mode"];
-        "modules-center" = [];
-        "modules-right" = ["battery" "network" "pulseaudio" "temperature" "cpu" "memory" "clock"];
-        "mpd" = {
-          "tooltip" = false;
-          "format" = "{stateIcon} {artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S})";
-          "format-disconnected" = "ﳌ";
-          "format-stopped" = "";
-          "state-icons" = {
-            "playing" = "";
-            "paused" = "";
-          };
-        };
+        "modules-left" = ["hyprland/workspaces"];
+        "modules-center" = ["hyprland/window"];
+        "modules-right" = ["backlight/slider" "battery" "bluetooth" "network" "pulseaudio" "temperature" "cpu" "memory" "clock"];
+
         "hyprland/workspaces" = {
           "format" = "{icon}";
           "format-icons" = {
@@ -46,6 +37,15 @@
             "default" = ["" "" "" "" ""];
           };
         };
+        "bluetooth" = {
+          "on-click" = "blueman-manager";
+          "format" = "{} {device_battery_percentage}";
+        };
+        "network" = {
+          "tooltip" = false;
+          "format-wifi" = "  {essid} {ipaddr}";
+          "format-ethernet" = "󰈀  {ipaddr}";
+        };
         "pulseaudio" = {
           "tooltip" = false;
           "scroll-step" = 5;
@@ -54,26 +54,45 @@
             "default" = ["" ""];
           };
         };
-        "network" = {
-          "tooltip" = false;
-          "format-wifi" = "  {essid} {ipaddr}";
-          "format-ethernet" = "󰈀  {ipaddr}";
-        };
         "cpu" = {
           "tooltip" = false;
-          "format" = "  {}%";
+          "format" = "  {usage}%";
         };
         "memory" = {
           "tooltip" = false;
-          "format" = "  {}%";
+          "format" = "  {used}/{total}GB";
         };
         "temperature" = {
           "tooltip" = false;
-          "format" = " {}°C";
+          "format" = " {temperatureC}°C";
+        };
+        "clock" = {
+          "format" = "{:%H:%M}";
+        };
+        "backlight/slider" = {
+          "min" = 1;
         };
       };
     };
     style = "
+      #backlight-slider slider {
+        min-height: 10px;
+        min-width: 10px;
+        background-image: none;
+        border: none;
+        box-shadow: none;
+      }
+      #backlight-slider trough {
+        min-height: 5px;
+        min-width: 80px;
+        border-radius: 5px;
+        background-color: black;
+      }
+      #backlight-slider highlight {
+        min-width: 10px;
+        border-radius: 5px;
+        background-color: cyan;
+      }
       #workspaces button {
         padding: 0 0.5em;
         background-color: @surface0;
@@ -81,13 +100,13 @@
         margin: 0.25em;
       }
       #workspaces button.empty {
-        color: #F8F8FF; /*white*/
+        color: gray;
       }
       #workspaces button.visible {
-        color: #ffffff; /*white*/
+        color: white;
       }
       #workspaces button.active {
-        color: #00FFFF; /*cyan*/
+        color: cyan;
       }
       #workspaces button.urgent {
         background-color: #DC143C; /*red*/
